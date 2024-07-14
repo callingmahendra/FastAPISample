f432ef787598d3cfe1edc7000e2f9e14a0f750a3
Mahendra Patel <30669870+callingmahendra@users.noreply.github.com>
2024-07-14 12:47:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_all_users(db: Session = Depends(get_db)):
 
 @app.get("/users/{user_id}")
 def get_user_by_user_id(user_id: int, db: Session = Depends(get_db)):
-    users = db.query(User).filter(User.id == user_id).first()
+    user = db.query(User).filter(User.id == user_id).first()
     if user:
         return user
     raise HTTPException(status_code=404, detail="User not found")
